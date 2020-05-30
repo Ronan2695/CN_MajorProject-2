de0101dddd35b049af34b9a2b2f4308e21b483d8
Ronan <rhtkmr2695@outlook.com>
2020-05-30 13:25:56 +0530

    Added controller and routes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
 const app = express();
-
 const port=8000; //In production, ports run on 80
+
+
+app.use('/', require('./routes/index'));
 
 app.listen(port, function(err){
 
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+//Entry Point To all the routes.
+
+const express = require('express');
+
+const router = express.Router();
+const homeController = require('../controllers/home_controller')
+
+console.log("router loaded");   
+
+router.get('/',homeController.home);
+
+router.get('/about',homeController.about);
+
+module.exports= router;
