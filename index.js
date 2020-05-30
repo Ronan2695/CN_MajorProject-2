add360a11998e97cc20f6bb0807dc530d190b46a
Ronan <rhtkmr2695@outlook.com>
2020-05-30 12:05:59 +0530

    First Commit: Create the directory structure and setup express sserver

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const port=8000; //In production, ports run on 80
+
+app.listen(port, function(err){
+
+    if(err)
+    {
+        console.log(`Error in running the server: ${err}`); //Interpolation
+    }
+
+    console.log(`Server is running on port:${port}`);
+
+})
+
+
+
