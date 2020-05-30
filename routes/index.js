b5029bfeabe175c3209dceda18e4b4310a1dd91f
Ronan <rhtkmr2695@outlook.com>
2020-05-30 13:47:46 +0530

    Added user routes and controllers

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,17 @@ const homeController = require('../controllers/home_controller')
 
 console.log("router loaded");   
 
+//homeroutes
 router.get('/',homeController.home);
-
 router.get('/about',homeController.about);
+
+//user routes
+router.use('/users', require('./users'))
+
+
+//for any further routes, access from here
+//router.use('/routerName', require('./routerfile'))
+
+
 
 module.exports= router;
