808813a9088e4de6baf63ed8f69d196e95e5c6cc
Ronan <rhtkmr2695@outlook.com>
2020-06-06 21:01:05 +0530

    Made changes to user.js router file

--- routes/users.js
+++ routes/users.js
@@ -10,15 +10,16 @@ router.get('/profile',usersController.profile);
 router.get('/post',postsController.posts);
 router.get('/edit',usersController.edit)
 
+//rendering the sign signup page
 router.get('/sign-in',usersController.signin)
 router.get('/sign-up',usersController.signup)
 
-//signup
+//signup(creating a user)
 router.post('/create',usersController.create)
 
 //use passport as a middleware to authenticate
 router.post('/create-session',passport.authenticate(
-    'local',
+    'local', //authenticatinf strategy is local
     {failureRedirect:'/users/sign-in'},
     ),usersController.createSession)
 
