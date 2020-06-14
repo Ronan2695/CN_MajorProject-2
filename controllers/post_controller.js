95dd8ef6abd0201ed8bd20eb9c7bfe3bb0ecde42
Ronan <rhtkmr2695@outlook.com>
2020-06-14 19:31:18 +0530

    Deleting posts along with comments

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post= require('../models/posts')
+const Comments = require('../models/comment');
 
 module.exports.create =  function(req,res){
 
@@ -19,3 +20,26 @@ module.exports.create =  function(req,res){
     });
      
 }
+
+
+// We creating an action for deleting posts and comments.
+module.exports.destroy =  function(req,res){
+    //finding whether post exists in a DB or not.
+    Post.findById(req.params.id, function(err,posts){
+        //authorization
+        //checking whether the user it is the user who was written the post
+        //.id means converting the object id into string.
+        if(posts.user == req.user.id)
+        {
+            post.remove();
+            Comments.deleteMany({post:req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        }
+        else
+        {
+            return res.redirect('back');     
+        }
+
+    });
+} 
