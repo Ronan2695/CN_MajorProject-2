f7dee4f3c6e89bbf05909afdd6947ed2ccbc45bf
Ronan <rhtkmr2695@outlook.com>
2020-06-14 15:51:12 +0530

    Added comments with username

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -13,7 +13,15 @@ module.exports.home= function(req,res)
 
     //we are populating the users from the User Schema 
     //Finding all the posts and populating user of each post
-    Post.find({}).populate('user').exec(function(err,posts  ){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+    .exec(function(err,posts  ){
         return res.render('home',{
             title:"Home",
             posts: posts
