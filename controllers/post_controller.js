ea5a8575e5462b1d4186f89373b8ab1f6fd80261
Ronan <rhtkmr2695@outlook.com>
2020-06-21 17:29:23 +0530

    Async and Await

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,47 +1,52 @@
 const Post= require('../models/posts')
 const Comments = require('../models/comment');
 
-module.exports.create =  function(req,res){
 
-    Post.create({
-        content:req.body.content,
-        user:req.user._id
+//Specifying async means we are declaring this function contains async statements 
+    module.exports.create =  async function(req,res){
 
-    }, function(err,post){
-        if(err)
-        {
-            console.log('error in creating a post');
-            return;
+        try{
 
+            await Post.create({
+                content:req.body.content,
+                user:req.user._id
+        
+            });
+            return res.redirect('back');
+
+        }catch(err){
+            console.log('Error', err);
+            return
         }
-
-        return res.redirect('back');
-
-    });
-     
+    
 }
 
 
 // We creating an action for deleting posts and comments.
+//Specifying async means we are declaring this function contains async statements.
 
-module.exports.destroy =  function(req,res){
-    //finding whether post exists in a DB or not.
-    Post.findById(req.params.id, function(err,post){
-        //authorization
-        //checking whether the user it is the user who was written the post
-        //.id means converting the object id into string.
-        
+    module.exports.destroy = async function(req,res){
+        //finding whether post exists in a DB or not.
+
+        try{
+
+        }catch(err){
+            console.log('Error', err);
+            return
+        }
+
+
+        let post = await Post.findById(req.params.id);
+
         if(post.user == req.user.id)
         {
             post.remove();
-            Comments.deleteMany({post:req.params.id}, function(err){
-                return res.redirect('back');
-            });
+            await Comments.deleteMany({post:req.params.id});    
+            return res.redirect('back');
         }
         else
         {
             return res.redirect('back');     
         }
 
-    });
-} 
+    } 
