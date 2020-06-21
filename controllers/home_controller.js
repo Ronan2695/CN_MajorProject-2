0bc53587aaed19e35f833b47c3c5f802cec55f18
Ronan <rhtkmr2695@outlook.com>
2020-06-21 15:58:29 +0530

    Converting home_controller to async

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,9 @@
 const Post = require('../models/posts');
 const User = require('../models/user');
 
-module.exports.home= function(req,res)
+
+//Specifying async means we are declaring this function contains async statements. 
+module.exports.home= async function(req,res)
 {
 
     // Post.find({}, function(err,posts){
@@ -12,32 +14,44 @@ module.exports.home= function(req,res)
 
     // });
 
-    //we are populating the users from the User Schema 
-    //Finding all the posts and populating user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-    })
-    .exec(function(err,posts){
+    // For Handling Errors 
 
-        User.find({}, function(err,users){
-            //We are sending all the posts and users. 
-            return res.render('home',{
-                title:"Home",
-                posts: posts,
-                all_users:users
-             });
-    
+    try{
+        //we are populating the users from the User Schema 
+    //Finding all the posts and populating user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user'
+            }
         });
         
+        let users = await User.find({});
         
-    })
+        //We are sending all the posts and users. 
+        return res.render('home',{
+            title:"Home",
+            posts: posts,
+            all_users:users
+        });
+
+
+    } catch(err){
+        console.log('Error',err);
+    }
+
     
 } 
+
+
+    //using then
+    //Post.find({}).populate('comments').then(function()); // this is not a promise
+    //let posts = Post.find({}).populate('comments').exec();
+    //When this query should be executed you can say posts.then()
+    //posts.then() // This is how a promise works
+
 
 //module.exports.actionName = function(req,res){}
 
