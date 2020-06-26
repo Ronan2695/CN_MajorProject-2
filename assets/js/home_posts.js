e954d6c6721ac87972acdd6a5a5c8b09795197a1
Ronan <rhtkmr2695@outlook.com>
2020-06-26 22:00:26 +0530

    Changes made Ajax Request for post

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -14,7 +14,8 @@
                     // this converts the form data into JSON
                     data:newPostForm.serialize(),
                     success: function(data){
-                        console.log(data)
+                        let newPost = newPostDom(data.data.post);
+                        $('#posts-list-container>ul').prepend(newPost);
                     }, error:function(error){
                         console.log(error.responseText);
                     }
@@ -24,8 +25,57 @@
 
 
 
-    createPost();
-}
+ 
+
 
 
 // method to create a POST in DOM
+
+let newPostDom=function(post){
+
+    return $(`<li id="post-${post._id}">
+            <p>
+        
+            <!-- Delete button for posts -->
+            <small> 
+                <a class="delete-post-button" href="/posts/destroy/${post._id}">X</a> 
+            </small>
+ 
+            ${post.content}
+            <br>
+            <small>
+            ${post.user.name}
+            </small>
+            </p>
+        <!-- Comments -->
+        <div class="post-comments">
+
+                <form action="/comments/create" method="POST">
+                    
+                    <input type="text" name="content" placeholder="Add Comment" required>
+                    <!-- Next we need to send the ID of the post to which the comment must be added. -->
+                    <input type="hidden" name="post" value="${post._id}">
+                    <input type="submit" value="Add Comment">
+
+                </form>
+                
+
+            <div class="post-comments-list">
+                <ul id="post-comments-${post._id}">
+
+                   
+
+                </ul>
+
+
+            </div>
+
+        </div>
+
+    </li>  `)
+
+
+    }
+    createPost();
+
+}
