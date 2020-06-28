85523be5ea290de084e8eaf2c027fdeda7244fc5
Ronan <rhtkmr2695@outlook.com>
2020-06-28 19:37:08 +0530

    Deleting a POST using API

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,8 +1,54 @@
-module.exports.index = function(req, res){
+const Post = require('../../../models/posts');
+const Comment = require ('../../../models/comment');
+
+module.exports.index = async function(req, res){
+
+    let posts = await Post.find({})
+    .sort('-createdAt')
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    });
+
 
     return res.json(200,{
         message: "List Of Posts",
-        posts: []
+        posts: posts
 
     })
 }
+
+
+module.exports.destroy = async function(req,res){
+    //finding whether post exists in a DB or not.
+
+        try{
+            let post = await Post.findById(req.params.id);
+
+            //  if(post.user == req.user.id) {
+                post.remove();
+                await Comment.deleteMany({post:req.params.id});   
+            
+
+                return res.json(200,{
+                    message:"Post and Associated Comments deleted successfully"
+                });
+            //} else
+            // {
+            //     req.flash('err', 'You cannot delete this POST');
+            //     return res.redirect('back');     
+            // }
+    
+
+        }catch(err){
+            console.log('*********',err)
+            return res.json(500,{
+                message: "Internal Server Error"
+            });   
+        }       
+
+    
+    } 
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const postsAPI = require("../../../controllers/api/v1/posts_api")
 router.get('/', postsAPI.index);
 
+//delete route
+router.delete('/:id', postsAPI.destroy);
+
 
 module.exports= router;
