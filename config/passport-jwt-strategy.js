c6c3fd47213441b6d86c3c440b78e112b9a2f483
Ronan <rhtkmr2695@outlook.com>
2020-06-29 23:09:09 +0530

    setting up JWT

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+//importing the strategy
+const JWTStrategy = require('passport-jwt').Strategy;
+//Exracting JWT from the header
+const ExtractJWT= require('passport-jwt').ExtractJwt;
+
+//We are requiring the users model.
+const User = require('../models/user');
+
+let opts = {
+
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken,
+    //This the encryption and decryption String
+    secretOrKey: 'codeial'
+
+}
+
+passport.use(new JWTStrategy(opts, function(jwtPayLoad, done){
+
+    User.findById(jwt.jwtPayLoad._id, function(err,user){
+        if(err)
+        {
+            console.log('Error in finding user from JWT');
+            return 
+        }
+
+        if(user)
+
+        {
+            return done(null,user); 
+        }else
+        return done ( null, false);
+    })
+
+}));
+
+module.exports = passport;
+
+
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const db= require('./config/mongoose')
 const session = require('express-session');
 const passport= require('passport');
 const passportLocal = require('./config/passport-local-strategy')
+const passportJWT= require('./config/passport-jwt-strategy')//requiring jwt module
 //This library requires an arguement for storing session information in the DB
 const MongoStore = require('connect-mongo')(session); //session arguement added since we need to store session 
 const sassMiddleware = require('node-sass-middleware');
