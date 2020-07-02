8a7e04fb9fcc2a17e8e293e534ee67dcafc9a634
Ronan <rhtkmr2695@outlook.com>
2020-07-02 21:40:45 +0530

    Adding config for nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,48 @@
+const nodemailer =  require('nodemailer');
+const ejs = require('ejs');
+const path= require('path');
+
+
+//Defining the mail communication
+let transporter = nodemailer.createTransport({
+
+    service:'gmail',
+    host:'smtp.gmail.com',
+    port:587,
+    secure: false,
+    auth:{
+        
+        user:'rhtkmr000@gmail.com',
+        pass:'RohitNannan@2695'
+    }
+
+});
+
+
+// We need to define EJS for the mail.
+ 
+let renderTemplate = (data, relativePath) => {
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailers', relativePath),
+        data,   
+        function(err,template){
+            if(err)
+            {
+                console.log('error in rendering template');
+                return
+            }
+            mailHTML = template;
+        }
+    )
+
+    return mailHTML;
+
+} 
+
+
+module.exports ={
+    transporter : transporter,
+    renderTemplate : renderTemplate
+
+}
