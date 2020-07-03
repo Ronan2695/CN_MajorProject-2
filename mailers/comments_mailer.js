bfe80125cf4c29adb5bc8a6088d397f7919b4435
Ronan <rhtkmr2695@outlook.com>
2020-07-03 23:51:03 +0530

    Added HTML template for mailers

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -3,7 +3,7 @@ const nodeMailer = require('../config/nodemailer');
 //this is another way of exporting a method
 exports.newComment = (comment) => {
 
-    console.log('inside newComment mailer');
+    let htmlString = nodeMailer.renderTemplate({comment:comment}, '/comments/new_comment.ejs')
 
     //sender mail details
     nodeMailer.transporter.sendMail({
@@ -12,7 +12,7 @@ exports.newComment = (comment) => {
         //sending it to the user who has commented
         to: comment.user.email,
         subject:"New Comment Published!",
-        html:'<h1>Yup, your comment is now published</h1>'
+        html:htmlString
     //call back incase there was an error
     },(err,info) => {
 
