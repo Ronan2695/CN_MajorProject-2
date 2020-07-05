040a24f5206a4f4635aa75b581fefe19719e0ada
Ronan <rhtkmr2695@outlook.com>
2020-07-06 01:09:33 +0530

    Setting up Chat Engine

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash'); // For Flash Messages
 const customMware= require('./config/middleware'); //requiring the custom middleware
 
+//Config for chat.Setting up the chat server,to be used with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('Chat server is listening on port 5000');
+
 
 app.use(sassMiddleware({
     src: './assets/scss',
