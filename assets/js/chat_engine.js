55ad1209ad141f32c1d3f5fc4189dd9bf87e2ba9
Ronan <rhtkmr2695@outlook.com>
2020-07-06 21:17:05 +0530

    Added Chatting Engine Config

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,10 +1,39 @@
 class ChatEngine{
 
-    constructor(chatBoxId,useremail){
-        
+    constructor(chatBoxId,userEmail){
+        this.chatBox= $('#${chatBoxId}');
+        this.userEmail=userEmail;
+
+        this.socket= io.connect('http://localhost:5000');
+
+        if (this.userEmail){
+            this.connectionHandler();
+        }
+    }
+
+    //creating a connection handler --> Will have to and fro interaction between server and client
+    connectionHandler(){
+
+        let self = this;
+
+        this.socket.on('connect', function(){
+            console.log('connection established using sockets....!')
 
 
+                self.socket.emit('join_room', {
+
+                    user_email:self.userEmail,
+                    chatroom:'codial'
+
+                });
+                
+                self.socket.on('user_joined', function(data){
+                    console.log('a user joined', data);
+
+                })
+        });
 
     }
+
 
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,5 +1,29 @@
+// Will receive the incoming connections
 module.exports.chatSockets = function(socketServer){
+    let io = require('socket.io')(socketServer);
+
+    io.sockets.on('connection', function(socket){
+        console.log('new connection received', socket.id);     
+        
+        //When client disconnects
+        socket.on('disconnect', function(){
+
+            console.log('socket disconnected');
 
 
+        });
+
+        socket.on('join_room', function(data){
+
+            console.log('Joining Request rec.', data);
+
+                socket.join(data.chatroom);
+
+                io.in(data.chatroom).emit('user_joined',data)
+
+            });
+
+        
+    });
 
 }
